84186dbf29415c705eac47d59404d14e3e14010f
Mazedur Rahman <mazedur.rahman.liton@gmail.com>
2018-08-31 01:53:21 -0700

    Added gorilla router for the simple web

--- simpleweb/server.go
+++ simpleweb/server.go
@@ -3,13 +3,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	//"time"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
-}
 
 func main(){
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Welcome to yamaszone!")
+		})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// s := &http.Server{
+	// 	Addr:           ":8081",
+	// 	//Handler:        myHandler,
+	// 	ReadTimeout:    10 * time.Second,
+	// 	WriteTimeout:   10 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
