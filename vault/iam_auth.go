4f79681e7debe51b416f7f97e4b88043ec308c1f
Mazedur Rahman <yamaszone@users.noreply.github.com>
2018-09-27 00:23:51 -0700

    Update iam_auth.go

--- vault/iam_auth.go
+++ vault/iam_auth.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"github.com/daveadams/onthelambda"
-	"log"
+        "fmt"
+        "github.com/daveadams/onthelambda"
+        "log"
 )
 
 func main() {
-	fmt.Println("Attempting authentication...")
-	client, err := onthelambda.VaultClient()
-	if err != nil {
-		log.Fatalf("ERROR: %s", err)
-	}
+        client, err := onthelambda.VaultClient()
+        if err != nil {
+                log.Fatalf("ERROR: %s", err)
+        }
 
-	//resp, _ := client.VaultAuth()
-	fmt.Println("Authentication success!")
-	fmt.Println(client)
+        fmt.Println(client.Token())
 }
